54c03acb2684ba91d9e48addd150c6674f45e058
Jerônimo Espala <jeronimo_espala@hotmail.com>
2022-11-04 08:23:39 -0300

    Configs for project

---
+++ paymentKafka/src/main/java/com/payment/controller/PaymentController.java
@@ -0,0 +1,13 @@
+package com.payment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    public String payment(@RequestBody String valor) {
+
+    }
+}
---
+++ paymentKafka/src/main/java/com/payment/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.payment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public void sendTopic() {
+
+    }
+}
